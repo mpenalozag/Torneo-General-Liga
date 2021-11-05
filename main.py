ab4f77bb15f53f066a6a0be64707c28bf21f5125
= <=>
2021-11-05 06:21:50 -0300

    Finished

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ participantes = []
 
 # Le pedimos al usuario que ingrese los nombres de cada participante.
 for i in range(cantidad_participantes):
-    nuevo_participante = input(f"Ingrese al participante número {i}\n")
+    nuevo_participante = input(f"Ingrese al participante número {i+1}\n")
     participantes.append(nuevo_participante)
     print("\n")
 
 # Creamos el torneo con los participantes.
 torneo = Tournament(participantes, cantidad_participantes)
-torneo.describeTournament()
+# Realizamos todos los enfrentamientos
+torneo.allMatchUps()
+# Printeamos la tabla del torneo
+torneo.printLeaderboard()
--- tournament.py
+++ tournament.py
@@ -8,24 +8,73 @@ class Tournament:
         self.participantes_sin_asignar = cantidad_participantes
         self.cantidad_participantes = cantidad_participantes
         self.points = {}
+        self.describeTournament()
+        self.setLeaderboard()
+
+    # Función para printear un Match Up
+
+    def printMatchUp(self, player_1, player_2):
+        print(" Match Up ")
+        print("----------")
+        print(f"{player_1} vs {player_2}\n")
+        print("----------")
+        print(f"Selecciona el resultado")
+        print(f"1) Gana {player_1}")
+        print(f"2) Gana {player_2}")
+        print("3) Empate\n")
 
 
     # Función que describe el torneo.
     def describeTournament(self):
-        print(f"Torneo con {self.cantidad_participantes}\n")
+        print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+        print(f"Torneo con {self.cantidad_participantes} participantes!")
+        print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n")
         print(" Participantes ")
         print("---------------")
         for i in range(self.cantidad_participantes):
             print(f"{i+1}) {self.participantes[i]}")
+        print("---------------\n\n\n")
 
     # Función que setea la tabla de posiciones.
     def setLeaderboard(self):
         # A cada participante le asignamos un espacio en el diccionario.
-        # El value asignado a cada participante corresponde a los puntos.
+        # El value asignado a cada participante corresponde a los puntos de ese participante.
         for participante in self.participantes:
-            pass
+            self.points[participante] = 0
 
 
     # Función que enfreta a 2 competidores y permite seleccionar quien gana
     def matchUp(self, player_1, player_2):
-        pass
+        # Printeamos el match up.
+        self.printMatchUp(player_1, player_2)
+        # Recibimos el resultado por parte del usuario.
+        resultado = int(input())
+        # Manejamos el resultado.
+        if (resultado == 1):
+            self.points[player_1] += 3
+        elif (resultado == 2):
+            self.points[player_2] += 3
+        else:
+            self.points[player_1] += 1
+            self.points[player_2] += 1
+
+    # Función que realiza todos los enfrentamientos del campeonato.
+    def allMatchUps(self):
+        # Definimos una lista de participantes que ya compitieron contra todos.
+        players_ready = []
+        # Recorremos todos los participantes.
+        for player_1 in self.participantes:
+            for player_2 in self.participantes:
+                # Si es que el participante no se ha enfrentado a todos entonces realizamos el matchUp
+                if (player_2 not in players_ready and player_1 != player_2):
+                    self.matchUp(player_1, player_2)
+            # Luego de haber hecho todos los matchUps, agregamos al player_1 a la lista de jugadores listos.
+            players_ready.append(player_1)
+    
+    # Función para imprimir la tabla de puntuación
+    def printLeaderboard(self):
+        print(" Tabla de la Liga ")
+        print("------------------")
+        # Recorremos el diccionario de puntos por participante.
+        for participante, puntos in self.points.items():
+            print ("{:<20} {:<5}".format(participante, puntos))
